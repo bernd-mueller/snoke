e60c3ca797c86c7fe30d793ebfb45b1d3c18b873
Bernd Müller <bernd.mueller@zbmed.de>
2020-11-24 17:08:30 +0100

    creation of ontology mapping file between the epilepsy ontologies based on their concepts and synonyms in the respective dictionaries

--- snoke.ontology/src/main/java/de/zbmed/snoke/ontology/analysis/DictLoader.java
+++ snoke.ontology/src/main/java/de/zbmed/snoke/ontology/analysis/DictLoader.java
@@ -23,6 +23,7 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.jena.ext.com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -78,6 +79,95 @@ public class DictLoader {
 		return vmap;
 	}
 	
+	public Map <String, Set <String>> getCodeMapForDict (String dictFile) {
+		Map <String, Set <String>> dictMap = new HashMap <String, Set <String>> ();
+		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = null;
+		
+		
+		log.info("Reading " + dictFile + "...");
+		try {
+			builder = builderFactory.newDocumentBuilder();
+
+			Document document = builder.parse(new FileInputStream(dictFile));
+			// new FileInputStream("/home/muellerb/test.xml"));
+
+			XPath xPath = XPathFactory.newInstance().newXPath();
+
+			String expression = "/synonym//token";
+			
+
+			// read a string value
+			// String email = xPath.compile(expression).evaluate(document);
+			NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(document, XPathConstants.NODESET);
+								
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				String codevalue = "";
+				Node n = nodeList.item(i);
+				
+				NamedNodeMap atts = n.getAttributes();
+				for (int j=0; j<atts.getLength(); j++) {
+					Node an = atts.item(j);
+					if (an.getNodeName().equals("CodeValue")) {
+						codevalue = an.getNodeValue();
+					}
+				}
+				Set <String> tokens = new HashSet <String> ();
+				NodeList nl = n.getChildNodes();
+				for (int j=0; j<nl.getLength(); j++) {
+					Node nj = nl.item(j);
+					if (nj.hasAttributes()) {
+						NamedNodeMap atts_nj = nj.getAttributes();
+						for (int k=0; k<atts_nj.getLength(); k++) {
+							Node an = atts_nj.item(k);
+							if (an.getNodeName().equals("base")) {
+								tokens.add(an.getNodeValue());
+							}
+						}
+					}
+				}
+				//System.out.println("i\t" + n.getNodeName());
+				if (codevalue.equals("")) {
+					dictMap.put(i+"", tokens);
+				} else {
+					dictMap.put(codevalue, tokens);
+				}
+			}
+			
+			log.info("" + dictMap.keySet().size());
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dictMap;
+	}
+	Map <String, Set <String>> createIntersection (Map <String, Set <String>> m1, Map <String, Set <String>> m2) {
+		Map <String, Set <String>> mintersection = new HashMap <String, Set <String>> ();
+		for (String k1 : m1.keySet()) {
+			Set <String> v1 = m1.get(k1);
+			for (String k2 : m2.keySet()) {
+				Set <String> v2 = m2.get(k2);
+				if (v1.contains(k2) | v2.contains(k1) | Sets.intersection(v1, v2).size()>0) {
+					mintersection.put(k1 + "#SAMEAS#" + k2, Sets.union(v1, v2));
+				}
+			}			
+			
+		}
+		
+		return mintersection;
+	}
+	
 	public Map <String, Set <String>> getMapForDict (String dictFile) {
 		Map <String, Set <String>> dictMap = new HashMap <String, Set <String>> ();
 		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
@@ -100,7 +190,7 @@ public class DictLoader {
 			// read a string value
 			// String email = xPath.compile(expression).evaluate(document);
 			NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(document, XPathConstants.NODESET);
-								
+			int valCounter = 0;					
 			for (int i = 0; i < nodeList.getLength(); i++) {
 				String conceptname = "";
 				Node n = nodeList.item(i);
@@ -122,6 +212,7 @@ public class DictLoader {
 							Node an = atts_nj.item(k);
 							if (an.getNodeName().equals("base")) {
 								tokens.add(an.getNodeValue());
+								valCounter++;
 							}
 						}
 					}
@@ -135,6 +226,7 @@ public class DictLoader {
 			}
 			
 			log.info("" + dictMap.keySet().size());
+			log.info("#Values: " + valCounter);
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
