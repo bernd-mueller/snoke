e6969653e5a397c19fcbee29fa56aeedc39ba491
Bernd Müller <bernd.mueller@zbmed.de>
2020-12-17 18:18:05 +0100

    icd rest client call for getting root entities stored in resources/icd/rootentities.txt

--- snoke.ontology/src/main/java/de/zbmed/snoke/ontology/icd/ICDAPIclient.java
+++ snoke.ontology/src/main/java/de/zbmed/snoke/ontology/icd/ICDAPIclient.java
@@ -5,15 +5,23 @@ import java.io.DataOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder; 
+import java.net.URLEncoder;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.net.ssl.HttpsURLConnection;
 
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 
 public class ICDAPIclient {
 
+	Set <String> rootents = new HashSet <String> ();
 	private final String TOKEN_ENPOINT = "https://icdaccessmanagement.who.int/connect/token";
 	private static String CLIENT_ID = "";
 	private static String CLIENT_SECRET = "";
@@ -43,7 +51,7 @@ public class ICDAPIclient {
 		ICDAPIclient api = new ICDAPIclient();
 		api.readCredentials();
 		String token = api.getToken();
-		System.out.println("URI Response JSON : \n" + api.getURI(token, uri));
+		System.out.println("URI Response JSON : \n" + api.getRootChildren(token, uri));
 	}
 
 
@@ -116,4 +124,51 @@ public class ICDAPIclient {
 		return response.toString();
 	}
 
+	// access ICD API
+	private String getRootChildren(String token, String uri) throws Exception {
+
+		System.out.println("Getting URI...");
+
+		URL url = new URL(uri);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+
+		// HTTP header fields to set
+		con.setRequestProperty("Authorization", "Bearer "+token);
+		con.setRequestProperty("Accept", "application/json");
+		con.setRequestProperty("Accept-Language", "en");
+		con.setRequestProperty("API-Version", "v2");
+
+		// response
+		int responseCode = con.getResponseCode();
+		System.out.println("URI Response Code : " + responseCode + "\n");
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		
+		while ((inputLine = in.readLine()) != null) {
+			System.out.println("####\t" + inputLine);
+			JSONTokener tokener = new JSONTokener(inputLine);
+			JSONObject root = new JSONObject(tokener);
+			System.out.println("child: " + root.has("child") + "\t" + root.get("child").getClass());
+			JSONArray children = (JSONArray)root.get("child");
+			
+			for (int i=0; i<children.length(); i++) {
+				rootents.add(children.getString(i).replace("http://id.who.int/icd/entity/", ""));
+			}
+			System.out.println("####\t" + children);
+			response.append(inputLine);
+		}
+		in.close();
+
+		PrintWriter writer = new PrintWriter("resources/icd/rootentities.txt", "UTF-8");
+		for (String ents : rootents) {
+			writer.println(ents);
+		}
+		writer.close();
+		return response.toString();
+	}
+	
 }
