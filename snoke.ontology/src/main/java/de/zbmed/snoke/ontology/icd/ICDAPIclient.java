d4cacce7f20352707965e85bbc60267ef66ce931
Bernd Müller <bernd.mueller@zbmed.de>
2020-12-18 13:37:12 +0100

    implementations for ICD dictionary construction

--- snoke.ontology/src/main/java/de/zbmed/snoke/ontology/icd/ICDAPIclient.java
+++ snoke.ontology/src/main/java/de/zbmed/snoke/ontology/icd/ICDAPIclient.java
@@ -17,17 +17,26 @@ import javax.net.ssl.HttpsURLConnection;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.w3c.dom.Element;
+
+import de.zbmed.snoke.ontology.common.DictHandler;
 
 
 public class ICDAPIclient {
 
+	Set <ICDentity> allents = new HashSet <ICDentity> ();
+	Set <String> processed = new HashSet <String> ();
 	Set <String> rootents = new HashSet <String> ();
+	int counter = 0;
+
 	private final String TOKEN_ENPOINT = "https://icdaccessmanagement.who.int/connect/token";
 	private static String CLIENT_ID = "";
 	private static String CLIENT_SECRET = "";
 	private final String SCOPE = "icdapi_access";
 	private final String GRANT_TYPE = "client_credentials";
-
+	ICDAPIclient () {
+		readCredentials ();
+	}
 
 	public void readCredentials () {
 		try (BufferedReader br = new BufferedReader(new FileReader("resources/credentials/creds.txt"))) {
@@ -43,17 +52,90 @@ public class ICDAPIclient {
 			e.printStackTrace();
 		}
 	}
+	
+	public void readRootEntities () {
+		try (BufferedReader br = new BufferedReader(new FileReader("resources/icd/rootentities.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				rootents.add(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) throws Exception {
-
 		
-		String uri = "https://id.who.int/icd/entity";
-
 		ICDAPIclient api = new ICDAPIclient();
-		api.readCredentials();
+
 		String token = api.getToken();
-		System.out.println("URI Response JSON : \n" + api.getRootChildren(token, uri));
+		api.readRootEntities();
+
+		int num = 0;
+		for (String rootids : api.getRootents()) {
+			System.out.println("Processing root id " + rootids + " " + ++num + "/" + api.getRootents().size());
+			int res = api.getEntityInformation(token, rootids);
+		}
+		api.writeToDict();
 	}
 
+	public void writeToDict () {
+		CreateDictFromICD dh = new CreateDictFromICD ();
+		dh.createConceptMapperDictionary(this.getAllents(), "dictionaries/Dict_ICD.xml", "ICD");
+	}
+
+	private int getEntityInformation(String token, String id) throws Exception {
+		if (!processed.contains(id)) {
+			if (++counter%100==0) {
+				System.out.println("Processing " + counter + "...");
+			}
+			ICDentity ie = new ICDentity();
+			// ie.setId(rootents.toArray()[0].toString());
+			// "250688797"
+			ie.setId(id);
+			//System.out.println("Rootents: " + rootents.size());
+			String uri = "https://id.who.int/icd/entity/" + ie.getId();
+			//System.out.println("Getting URI...");
+	
+			URL url = new URL(uri);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+	
+			// HTTP header fields to set
+			con.setRequestProperty("Authorization", "Bearer " + token);
+			con.setRequestProperty("Accept", "application/json");
+			con.setRequestProperty("Accept-Language", "en");
+			con.setRequestProperty("API-Version", "v2");
+	
+			// response
+			
+			int responseCode = con.getResponseCode();
+			// System.out.println("URI Response Code : " + responseCode + "\n");
+	
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+	
+			while ((inputLine = in.readLine()) != null) {
+	
+				ie.processJSON(inputLine);
+				response.append(inputLine);
+			}
+			in.close();
+	
+			//System.out.println("###" + ie.toString());
+			this.getAllents().add(ie);
+	
+			for (String identifier : ie.getChildren()) {
+				this.getEntityInformation(token, identifier);
+			}
+			processed.add(id);
+			return responseCode;
+		} else {
+			System.out.println("Duplicate id: " + id);
+			return -1;
+		}
+	}
 
 	// get the OAUTH2 token
 	private String getToken() throws Exception {
@@ -95,9 +177,9 @@ public class ICDAPIclient {
 
 
 	// access ICD API
-	private String getURI(String token, String uri) throws Exception {
-
-		System.out.println("Getting URI...");
+	private String getURI(String token) throws Exception {
+		String uri = "https://id.who.int/icd/entity";
+		// System.out.println("Getting URI...");
 
 		URL url = new URL(uri);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -111,7 +193,7 @@ public class ICDAPIclient {
 
 		// response
 		int responseCode = con.getResponseCode();
-		System.out.println("URI Response Code : " + responseCode + "\n");
+		// System.out.println("URI Response Code : " + responseCode + "\n");
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
@@ -125,9 +207,9 @@ public class ICDAPIclient {
 	}
 
 	// access ICD API
-	private String getRootChildren(String token, String uri) throws Exception {
-
-		System.out.println("Getting URI...");
+	private String getRootChildren(String token) throws Exception {
+		String uri = "https://id.who.int/icd/entity";
+		
 
 		URL url = new URL(uri);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -142,33 +224,49 @@ public class ICDAPIclient {
 		// response
 		int responseCode = con.getResponseCode();
 		System.out.println("URI Response Code : " + responseCode + "\n");
-
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		String inputLine;
 		StringBuffer response = new StringBuffer();
+		if (responseCode == 200) {
+	
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
 
-		
-		while ((inputLine = in.readLine()) != null) {
-			System.out.println("####\t" + inputLine);
-			JSONTokener tokener = new JSONTokener(inputLine);
-			JSONObject root = new JSONObject(tokener);
-			System.out.println("child: " + root.has("child") + "\t" + root.get("child").getClass());
-			JSONArray children = (JSONArray)root.get("child");
+	
 			
-			for (int i=0; i<children.length(); i++) {
-				rootents.add(children.getString(i).replace("http://id.who.int/icd/entity/", ""));
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println("####\t" + inputLine);
+				JSONTokener tokener = new JSONTokener(inputLine);
+				JSONObject root = new JSONObject(tokener);
+				System.out.println("child: " + root.has("child") + "\t" + root.get("child").getClass());
+				JSONArray children = (JSONArray)root.get("child");
+				
+				for (int i=0; i<children.length(); i++) {
+					rootents.add(children.getString(i).replace("http://id.who.int/icd/entity/", ""));
+				}
+				System.out.println("####\t" + children);
+				response.append(inputLine);
 			}
-			System.out.println("####\t" + children);
-			response.append(inputLine);
-		}
-		in.close();
-
-		PrintWriter writer = new PrintWriter("resources/icd/rootentities.txt", "UTF-8");
-		for (String ents : rootents) {
-			writer.println(ents);
-		}
-		writer.close();
+			in.close();
+	
+			PrintWriter writer = new PrintWriter("resources/icd/rootentities.txt", "UTF-8");
+			for (String ents : rootents) {
+				writer.println(ents);
+			}
+			writer.close();
+			}
 		return response.toString();
 	}
-	
+	public Set<ICDentity> getAllents() {
+		return allents;
+	}
+
+	public void setAllents(Set<ICDentity> allents) {
+		this.allents = allents;
+	}
+	public Set<String> getRootents() {
+		return rootents;
+	}
+
+	public void setRootents(Set<String> rootents) {
+		this.rootents = rootents;
+	}
 }
--- snoke.ontology/src/main/java/de/zbmed/snoke/ontology/icd/ICDentity.java
+++ snoke.ontology/src/main/java/de/zbmed/snoke/ontology/icd/ICDentity.java
@@ -3,9 +3,27 @@ package de.zbmed.snoke.ontology.icd;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class ICDentity {
+	String id = "";
+	String parent = "";
+	String title = "";
+	Set <String> children = new HashSet<String> ();
+	Set <String> synonyms = new HashSet<String> ();
+	
+	public Set<String> getSynonyms() {
+		return synonyms;
+	}
+
+	public void setSynonyms(Set<String> synonyms) {
+		this.synonyms = synonyms;
+	}
+
+
+
 	public String getId() {
 		return id;
 	}
@@ -37,13 +55,44 @@ public class ICDentity {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-	String id = "";
-	String parent = "";
-	Set <String> children = new HashSet<String> ();
-	String title = "";
-
-	public void processJSON (JSONObject j) {
-		
+	
+	public void processJSON (String jsonstring) {
+		JSONTokener tokener = new JSONTokener(jsonstring);
+		JSONObject root = new JSONObject(tokener);
+		JSONTokener titletokener = new JSONTokener(root.get("title").toString());
+		JSONObject tj = new JSONObject(titletokener);		
+		this.setTitle(tj.getString("@value"));
+		if (root.has("child")) {
+			JSONArray children = (JSONArray)root.get("child");
+			for (int i=0; i<children.length(); i++) {
+				this.getChildren().add(children.getString(i).replace("http://id.who.int/icd/entity/", ""));
+			}
+		}
+		if (root.has("synonym")) {
+			JSONArray synonyms = (JSONArray)root.get("synonym");
+			for (int i=0; i<synonyms.length(); i++) {
+				JSONTokener labletokener = new JSONTokener(synonyms.get(i).toString());
+				JSONObject lj = new JSONObject(labletokener);	
+				JSONTokener valtokener = new JSONTokener(lj.get("label").toString());
+				JSONObject vj = new JSONObject(valtokener);
+				String syn = vj.get("@value").toString();
+				this.getSynonyms().add(syn);
+			}
+		}
+	}
+	
+	public String toString () {
+		String res = "";
+		res += "ID:\t" + this.getId() + "\n";
+		res += "Title:\t" + this.getTitle() + "\n";
+		res += "Synonyms:\t" + "\n";
+		for (String s : this.getSynonyms()) {
+			res += "\t" + s + "\n";
+		}
+		res += "Children:\t" + "\n";
+		for (String s : this.getChildren()) {
+			res += "\t" + s + "\n";
+		}
+		return res;
 	}
 }
