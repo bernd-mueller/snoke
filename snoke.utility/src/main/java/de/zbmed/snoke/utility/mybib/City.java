ca2f6b7ef9d9da2dd48afa7b8cb7345c6dfc3aab
Bernd Müller <bernd.mueller@zbmed.de>
2021-04-12 08:39:23 +0200

    classes for processing mybib data

---
+++ snoke.utility/src/main/java/de/zbmed/snoke/utility/mybib/City.java
@@ -0,0 +1,45 @@
+package de.zbmed.snoke.utility.mybib;
+
+public class City {
+	String stadt, plz, bland, kreis, key;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getStadt() {
+		return stadt;
+	}
+
+	public void setStadt(String stadt) {
+		this.stadt = stadt;
+	}
+
+	public String getPlz() {
+		return plz;
+	}
+
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+
+	public String getBland() {
+		return bland;
+	}
+
+	public void setBland(String bland) {
+		this.bland = bland;
+	}
+
+	public String getKreis() {
+		return kreis;
+	}
+
+	public void setKreis(String kreis) {
+		this.kreis = kreis;
+	}
+}
