c9741020be1d1f0328709730733209a2ed77b12b
Bernd Müller <bernd.mueller@zbmed.de>
2021-11-08 15:03:43 +0100

    swaggedi swag

---
+++ snoke.swag/src/main/java/de/zbmed/snoke/swag/api/MyAPI.java
@@ -0,0 +1,14 @@
+package de.zbmed.snoke.swag.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyAPI {
+	private static final Logger log = LoggerFactory.getLogger(MyAPI.class);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log.debug("I am an API");
+
+	}
+
+}
---
+++ snoke.swag/src/main/java/de/zbmed/snoke/swag/rest/apiREST.java
@@ -0,0 +1,14 @@
+package de.zbmed.snoke.swag.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class apiREST {
+	private static final Logger log = LoggerFactory.getLogger(apiREST.class);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log.debug("I'm a REST-API");
+	}
+
+}
