c35f16c807061eefce3641a848865f294c5c2615
Bernd Müller <bernd.mueller@zbmed.de>
2022-02-04 13:00:19 +0100

    cleared warning messages

--- snoke.dl/src/main/java/de/zbmed/snoke/dl/doc2vec/BioASQPredictor.java
+++ snoke.dl/src/main/java/de/zbmed/snoke/dl/doc2vec/BioASQPredictor.java
@@ -61,7 +61,7 @@ public class BioASQPredictor {
             log.info("Reading Mapping file " + mapfilename);
 
             String line = reader.readLine();
-            int counter = 0;
+
             while(line != null){
                 line = reader.readLine();
                 if (line!= null) {
@@ -75,6 +75,7 @@ public class BioASQPredictor {
                 }
             }
 
+            reader.close();
             log.info("Done reading mapping file...");
 
 
