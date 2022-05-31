1f09d285f92efd427fa1e69bd7e05a0390f09276
Bernd Müller <bernd.mueller@zbmed.de>
2022-05-31 09:26:18 +0200

    scaffold for OHD dict generation

---
+++ snoke.ontology/src/main/java/de/zbmed/snoke/ontology/ohd/CreateDictFromOHD.java
@@ -0,0 +1,18 @@
+package de.zbmed.snoke.ontology.ohd;
+
+import java.util.Set;
+
+import org.apache.jena.ontology.OntClass;
+
+import de.zbmed.snoke.ontology.common.DictHandler;
+
+
+public class CreateDictFromOHD extends DictHandler {
+
+	@Override
+	public Set<String> processPropertySeeAlso(OntClass oc, Set<String> synset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
