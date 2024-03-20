cb12ec834e22550c4a26896376a2424d2f875a20
Bernd Müller <bernd.mueller@zbmed.de>
2024-03-20 08:14:20 +0100

    BarnesHutTsne import has broken dependency

--- snoke.dl/src/main/java/de/zbmed/snoke/dl/vis/VtSNE.java
+++ snoke.dl/src/main/java/de/zbmed/snoke/dl/vis/VtSNE.java
@@ -3,7 +3,7 @@ package de.zbmed.snoke.dl.vis;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
-import org.deeplearning4j.plot.BarnesHutTsne;
+//import org.deeplearning4j.plot.BarnesHutTsne;
 import org.deeplearning4j.nlp.uima.tokenization.tokenizer.preprocessor.StemmingPreprocessor;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
@@ -61,21 +61,21 @@ public class VtSNE {
 
             //STEP 3: build a dual-tree tsne to use later
             log.info("Build model....");
-            BarnesHutTsne tsne = new BarnesHutTsne.Builder()
-                .setMaxIter(iterations).theta(0.5)
-                .normalize(false)
-                .learningRate(500)
-                .useAdaGrad(false)
-//                .usePca(false)
-                .build();
-
-            //STEP 4: establish the tsne values and save them to a file
-            log.info("Store TSNE Coordinates for Plotting....");
-            String outputFile = outfile;
-            (new File(outputFile)).getParentFile().mkdirs();
-            tsne.plot(weights,2,cacheList,outputFile);
-            //This tsne will use the weights of the vectors as its matrix, have two dimensions, use the words strings as
-            //labels, and be written to the outputFile created on the previous line
+//            BarnesHutTsne tsne = new BarnesHutTsne.Builder()
+//                .setMaxIter(iterations).theta(0.5)
+//                .normalize(false)
+//                .learningRate(500)
+//                .useAdaGrad(false)
+////                .usePca(false)
+//                .build();
+//
+//            //STEP 4: establish the tsne values and save them to a file
+//            log.info("Store TSNE Coordinates for Plotting....");
+//            String outputFile = outfile;
+//            (new File(outputFile)).getParentFile().mkdirs();
+//            tsne.plot(weights,2,cacheList,outputFile);
+//            //This tsne will use the weights of the vectors as its matrix, have two dimensions, use the words strings as
+//            //labels, and be written to the outputFile created on the previous line
 
         } catch (IOException e) {
             e.printStackTrace();
